64f6545c14d1477d3a7b9e98b5f2ede4e28b866d
Administrator <Administrator@PC-20140113JAKV>
2016-12-28 10:53:09 +0800

    基本信息复选框

--- src/main/java/org/honor/tourism/controller/SelfSupportRouteBaseInfoController.java
+++ src/main/java/org/honor/tourism/controller/SelfSupportRouteBaseInfoController.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
