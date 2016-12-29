b390b64a2acc27f766ff3004ced6f8ddd008af1d
keiwu <keiwu@LAPTOP-54NDCEOC>
2016-12-29 10:09:41 +0800

    后台获取当前登录用户

--- src/main/java/org/honor/tourism/service/SysUserService.java
+++ src/main/java/org/honor/tourism/service/SysUserService.java
@@ -6,4 +6,6 @@ public interface SysUserService {
 
 	public SysUser save(SysUser sysUser);
 	
+	public SysUser findByLogin();
+	
 }
--- src/main/java/org/honor/tourism/service/impl/SysUserServiceImpl.java
+++ src/main/java/org/honor/tourism/service/impl/SysUserServiceImpl.java
@@ -4,6 +4,8 @@ import org.honor.tourism.entity.SysUser;
 import org.honor.tourism.repository.SysUserRepository;
 import org.honor.tourism.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +17,15 @@ public class SysUserServiceImpl implements SysUserService {
 	@Override
 	public SysUser save(SysUser sysUser) {
 		return repository.save(sysUser);
+	}
+
+	@Override
+	public SysUser findByLogin() {
+		UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext()
+			    .getAuthentication()
+			    .getPrincipal();
+		String username = userDetails.getUsername();
+		return repository.findByUsername(username);
 	}
 
 }
