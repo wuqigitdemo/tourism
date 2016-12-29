bb7d52644e844868a346cbe16126dd16fd5b9ade
keiwu <keiwu@LAPTOP-54NDCEOC>
2016-12-29 14:17:15 +0800

    添加json单元测试方式

--- src/test/java/org/honor/tourism/controller/TestTourismThemeController.java
+++ src/test/java/org/honor/tourism/controller/TestTourismThemeController.java
@@ -3,12 +3,16 @@ package org.honor.tourism.controller;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.honor.tourism.Application;
+import org.honor.tourism.entity.TourismTheme;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +21,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -38,6 +44,24 @@ public class TestTourismThemeController {
 		MvcResult result = mockMvc.perform(
 			MockMvcRequestBuilders.post("/TourismTheme/save")
 				.param("themeName", "单元测试旅游主题"))
+				.andDo(MockMvcResultHandlers.print())
+				.andReturn();
+		 assertEquals("{\"success\":true}", result.getResponse().getContentAsString());
+	}
+	
+	/**
+	 * json提交方式
+	 * @throws Exception
+	 */
+	public void testSave2() throws Exception {
+		TourismTheme tt = new TourismTheme();
+		tt.setThemeName("&&**((");
+		ObjectMapper mapper = new ObjectMapper();  
+		MvcResult result = mockMvc.perform(
+			MockMvcRequestBuilders.post("/TourismTheme/save")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(mapper.writeValueAsString(tt))
+				)
 				.andDo(MockMvcResultHandlers.print())
 				.andReturn();
 		 assertEquals("{\"success\":true}", result.getResponse().getContentAsString());
