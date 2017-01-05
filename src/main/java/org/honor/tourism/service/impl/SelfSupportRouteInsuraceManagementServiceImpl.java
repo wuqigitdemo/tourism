30f21214b15ffa161822e5144fd109aab5bb01fa
xiuluodaren <xiuluodaren@xiuluodaren>
2017-01-05 17:23:30 +0800

    自营线路保险修改

--- src/main/java/org/honor/tourism/service/impl/SelfSupportRouteInsuraceManagementServiceImpl.java
+++ src/main/java/org/honor/tourism/service/impl/SelfSupportRouteInsuraceManagementServiceImpl.java
@@ -1,5 +1,6 @@
 package org.honor.tourism.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,6 +11,7 @@ import org.honor.tourism.entity.SelfSupportRoute;
 import org.honor.tourism.repository.InsuranceManagementRepository;
 import org.honor.tourism.repository.SelfSupportRouteRepository;
 import org.honor.tourism.service.SelfSupportRouteInsuraceManagementService;
+import org.honor.tourism.util.EasyuiPage;
 import org.honor.tourism.util.EasyuiResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -40,10 +42,10 @@ public class SelfSupportRouteInsuraceManagementServiceImpl implements SelfSuppor
 	public Map<String, Object> addInsuranceManagement(String imId,String routeId) {
 		
 		//查询保险
-		InsuranceManagement insuranceManagement = insuranceManagementRepository.getOne(imId);
+		InsuranceManagement insuranceManagement = insuranceManagementRepository.findOne(imId);
 		
 		//查询线路
-		SelfSupportRoute selfSupportRoute = selfSupportRouteRepository.getOne(routeId);
+		SelfSupportRoute selfSupportRoute = selfSupportRouteRepository.findOne(routeId);
 		
 		//线路保险列表
 		List<InsuranceManagement> insuranceManagementList = selfSupportRoute.getInsuranceManagementList();
@@ -73,5 +75,77 @@ public class SelfSupportRouteInsuraceManagementServiceImpl implements SelfSuppor
 		}
 		
 		return EasyuiResult.result(true,"操作成功");
+	}
+
+	/**
+	 * 查询线路保险
+	 * @param routeId
+	 * @return
+	 */
+	@Override
+	public List<InsuranceManagement> findList(EasyuiPage page,String routeId)
+	{
+		//查询线路
+		SelfSupportRoute selfSupportRoute = selfSupportRouteRepository.findOne(routeId);
+
+		//线路保险列表
+		List<InsuranceManagement> list = selfSupportRoute.getInsuranceManagementList();
+
+		//分页
+		Integer startIndex = page.getPage() * page.getRows();
+		Integer endIndex = (page.getPage() + 1) * page.getRows();
+		
+		endIndex = endIndex > 0 ? endIndex - 1 : 0;
+		
+		List<InsuranceManagement> rows = new ArrayList<InsuranceManagement>();
+		
+		for (int i = 0;i < list.size();i++) {
+			if (i >= startIndex && i <= endIndex) {
+				rows.add(list.get(i));
+			}
+		}
+		
+		return rows;
+	}
+
+	/**
+	 * 删除保险
+	 * @param imId
+	 * @param routeId
+	 * @return
+	 */
+	@Override
+	public SelfSupportRoute deleteInsuranceManagement(String imId, String routeId)
+	{
+		//查询线路
+		SelfSupportRoute selfSupportRoute = selfSupportRouteRepository.findOne(routeId);
+		
+		//线路保险列表
+		List<InsuranceManagement> insuranceManagements = selfSupportRoute.getInsuranceManagementList();
+		
+		for (InsuranceManagement insuranceManagement : insuranceManagements) {
+			if (insuranceManagement.getId().equals(imId)) {
+				insuranceManagements.remove(insuranceManagement);
+				break;
+			}
+		}
+		
+		return selfSupportRouteRepository.save(selfSupportRoute);
+	}
+	
+	/**
+	 * 线路中保险个数
+	 * @param routeId
+	 * @return
+	 */
+	@Override
+	public Integer imCount(String routeId) {
+		//查询线路
+		SelfSupportRoute selfSupportRoute = selfSupportRouteRepository.findOne(routeId);
+		
+		//线路保险列表
+		List<InsuranceManagement> insuranceManagements = selfSupportRoute.getInsuranceManagementList();
+		
+		return insuranceManagements.size();
 	}
 }
