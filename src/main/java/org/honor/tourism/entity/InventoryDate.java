2fd9c82859c4474da51964a902cb96e73b1a3eff
keiwu <keiwu@LAPTOP-54NDCEOC>
2017-01-05 10:19:55 +0800

    修改库存价格

--- src/main/java/org/honor/tourism/entity/InventoryDate.java
+++ src/main/java/org/honor/tourism/entity/InventoryDate.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
 
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.validator.constraints.Length;
@@ -32,6 +33,21 @@ public class InventoryDate {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date inventoryDate;
+	/** 成人售价 */
+	@Min(value = 0, message = "成人售价必须大于0")
+	private Double adultPrice;
+	/** 折扣价 */
+	@Min(value = 0, message = "折扣价必须大于0")
+	private Double discountPrice;
+	/** 儿童售价 */
+	@Min(value = 0, message = "儿童售价必须大于0")
+	private Double childrenPrice;
+	/** 儿童折扣价 */
+	@Min(value = 0, message = "儿童折扣价必须大于0")
+	private Double childrenDiscountPrice;
+	/** 库存(人) */
+	@Min(value = 0, message = "库存必须大于0")
+	private Integer inventoryPerson;
 
 	public String getId() {
 		return id;
@@ -48,5 +64,45 @@ public class InventoryDate {
 	public void setInventoryDate(Date inventoryDate) {
 		this.inventoryDate = inventoryDate;
 	}
-	
+
+	public Double getAdultPrice() {
+		return adultPrice;
+	}
+
+	public void setAdultPrice(Double adultPrice) {
+		this.adultPrice = adultPrice;
+	}
+
+	public Double getDiscountPrice() {
+		return discountPrice;
+	}
+
+	public void setDiscountPrice(Double discountPrice) {
+		this.discountPrice = discountPrice;
+	}
+
+	public Double getChildrenPrice() {
+		return childrenPrice;
+	}
+
+	public void setChildrenPrice(Double childrenPrice) {
+		this.childrenPrice = childrenPrice;
+	}
+
+	public Double getChildrenDiscountPrice() {
+		return childrenDiscountPrice;
+	}
+
+	public void setChildrenDiscountPrice(Double childrenDiscountPrice) {
+		this.childrenDiscountPrice = childrenDiscountPrice;
+	}
+
+	public Integer getInventoryPerson() {
+		return inventoryPerson;
+	}
+
+	public void setInventoryPerson(Integer inventoryPerson) {
+		this.inventoryPerson = inventoryPerson;
+	}
+
 }
--- src/main/java/org/honor/tourism/entity/PriceInventory.java
+++ src/main/java/org/honor/tourism/entity/PriceInventory.java
@@ -89,21 +89,6 @@ public class PriceInventory {
 	/** 签证中心-分配总价 */
 	@Min(value = 0, message = "签证中心-分配总价必须大于0")
 	private Double visaCenterAllocationTotalPrice;
-	/** 成人售价 */
-	@Min(value = 0, message = "成人售价必须大于0")
-	private Double adultPrice;
-	/** 折扣价 */
-	@Min(value = 0, message = "折扣价必须大于0")
-	private Double discountPrice;
-	/** 库存(人) */
-	@Min(value = 0, message = "库存必须大于0")
-	private Integer inventoryPerson;
-	/** 儿童售价 */
-	@Min(value = 0, message = "儿童售价必须大于0")
-	private Double childrenPrice;
-	/** 儿童折扣价 */
-	@Min(value = 0, message = "儿童折扣价必须大于0")
-	private Double childrenDiscountPrice;
 	/** 占床加价 */
 	@Min(value = 0, message = "占床加价必须大于0")
 	private Double priceOfBed;
@@ -308,38 +293,6 @@ public class PriceInventory {
 		this.visaCenterAllocationTotalPrice = visaCenterAllocationTotalPrice;
 	}
 
-	public Double getAdultPrice() {
-		return adultPrice;
-	}
-
-	public void setAdultPrice(Double adultPrice) {
-		this.adultPrice = adultPrice;
-	}
-
-	public Double getDiscountPrice() {
-		return discountPrice;
-	}
-
-	public void setDiscountPrice(Double discountPrice) {
-		this.discountPrice = discountPrice;
-	}
-
-	public Integer getInventoryPerson() {
-		return inventoryPerson;
-	}
-
-	public void setInventoryPerson(Integer inventoryPerson) {
-		this.inventoryPerson = inventoryPerson;
-	}
-
-	public Double getChildrenPrice() {
-		return childrenPrice;
-	}
-
-	public void setChildrenPrice(Double childrenPrice) {
-		this.childrenPrice = childrenPrice;
-	}
-
 	public Double getPriceOfBed() {
 		return priceOfBed;
 	}
@@ -434,14 +387,6 @@ public class PriceInventory {
 
 	public void setWithTheIndustryChildrensPriceDescription(String withTheIndustryChildrensPriceDescription) {
 		this.withTheIndustryChildrensPriceDescription = withTheIndustryChildrensPriceDescription;
-	}
-
-	public Double getChildrenDiscountPrice() {
-		return childrenDiscountPrice;
-	}
-
-	public void setChildrenDiscountPrice(Double childrenDiscountPrice) {
-		this.childrenDiscountPrice = childrenDiscountPrice;
 	}
 
 	public Integer getChildrenDefineStandardValue() {
