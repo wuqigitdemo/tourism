0534498d61f88b3cdec82d2ff478cc7d95052c5a
keiwu <keiwu@LAPTOP-54NDCEOC>
2017-01-19 14:58:30 +0800

    添加id检索方法

--- src/main/java/org/honor/tourism/repository/RouteTripRepository.java
+++ src/main/java/org/honor/tourism/repository/RouteTripRepository.java
@@ -1,6 +1,8 @@
 package org.honor.tourism.repository;
 
 import org.honor.tourism.entity.RouteTrip;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -10,4 +12,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RouteTripRepository extends JpaRepository<RouteTrip, String> {
 
+	public Page<RouteTrip> findBySelfSupportRouteId(Pageable pageable, String selfSupportRouteId);
+	
 }
