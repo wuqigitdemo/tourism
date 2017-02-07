ff83dc9388100558091c8def4e40a2612860af36
keiwu <keiwu@LAPTOP-54NDCEOC>
2017-02-07 10:08:29 +0800

    添加酒店级别字段

--- src/main/java/org/honor/tourism/entity/HotelLevel.java
+++ src/main/java/org/honor/tourism/entity/HotelLevel.java
@@ -6,19 +6,40 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * 酒店级别
+ * 
  * @author keiwu
  *
  */
 @Entity
 @Table(name = "t_hotel_level")
 public class HotelLevel {
-	
+
 	@Id
 	@GeneratedValue(generator = "uuid")
 	@GenericGenerator(name = "uuid", strategy = "uuid")
 	private String id;
+	/** 级别名称 */
+	@Length(min = 1, max = 30, message = "级别名称需要1-30位字符")
+	private String levelName;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLevelName() {
+		return levelName;
+	}
+
+	public void setLevelName(String levelName) {
+		this.levelName = levelName;
+	}
 
 }
