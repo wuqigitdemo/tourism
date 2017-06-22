4febf8858cde67d71a2c475c5649ab80764d120b
xiuluodaren <xiuluodaren@localhost>
2017-06-22 15:01:08 +0800

    添加外部容器部署

--- src/main/java/org/honor/tourism/Application.java
+++ src/main/java/org/honor/tourism/Application.java
@@ -3,13 +3,14 @@ package org.honor.tourism;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 
 
 @Controller
 @EnableAutoConfiguration
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
--- src/main/java/org/honor/tourism/entity/Module.java
+++ src/main/java/org/honor/tourism/entity/Module.java
@@ -3,6 +3,7 @@ package org.honor.tourism.entity;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
